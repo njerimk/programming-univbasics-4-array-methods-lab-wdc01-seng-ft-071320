5d7c5d90fd472cd1c54166c761b84847c10367c4
Njeri W Murugi-Kamau <nmurugikamau@gmail.com>
2020-07-04 00:08:42 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,29 @@
 def using_include(array, element)
-
+  array.include?(element)
 end
 
 def using_sort(array)
-
+  array=["I","bananas","wow"]
+  array.sort
 end
 
 def using_reverse(array)
+  array=["wow","arrays!"]
+  array.reverse
 
 end
 
 def using_first(array)
-
+  array=["wow", "arrays!"]
+  array.first
 end
 
 def using_last(array)
-
+  array=["bike","arrays!"]
+  array.last
 end
 
 def using_size(array)
-
+  array=["sailor mars","sailor mercury","sailor jupiter", "sailor moon", "sailor venus", "sailor pluto"]
+  array.size
 end
